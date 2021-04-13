b32acd26812c11bfd2821a0a9c4049cdf41d84dc
Abel <abelper54@gmail.com>
2021-04-12 22:28:37 -0400

    added multi scos support

--- src/scorm-creator.js
+++ src/scorm-creator.js
@@ -1,9 +1,9 @@
 import jszip from "jszip"
 import saveAs from "file-saver"
 
-export default function createScorm(title, subtitle, path, version = "1.0", org = "unfoldingWord") {
+export function createScorm(title, subtitle, path, org = "unfoldingWord", lang = "en", version = "1.0") {
     
-    const url = `https://git.door43.org/unfoldingWord/en_ta/raw/branch/master/${path}/01.md`
+    const url = `https://git.door43.org/${org}/${lang}_ta/raw/branch/master/${path}/01.md`
     const courseName = path.split('/').join('_')
 
     fetch('./src/scorm.zip')       // 1) fetch the url
@@ -61,9 +61,140 @@ export default function createScorm(title, subtitle, path, version = "1.0", org 
                 zip.file("imsmanifest.xml", newManifest)
             }
 
-            zip.generateAsync({type:"blob"}).then(function (blob) {
-                const zipName = `${courseName}.sco.zip`
+            zip.generateAsync({type:"blob", compression: "DEFLATE", compressionOptions: { level: 9 }}).then(function (blob) {
+                const zipName = `${courseName}.pif.zip`
                 saveAs(blob, zipName)
             })      
         })
 } 
+
+
+
+function fileNameEncode(str){
+    return str.toLowerCase().split(' ').join("-")
+}
+
+
+async function addSCOs(section, topic, manifest, html, zip, org, lang){
+ 
+
+    if(section.hasOwnProperty('link')){
+
+        let filePath = `resources/${topic}/${section.link}.html`
+        let url = `https://git.door43.org/${org}/${lang}_ta/raw/branch/master/${topic}/${section.link}/`   
+
+        let file = manifest.createElement('file')
+            file.setAttribute('href', filePath)
+
+        let resource =  manifest.createElement('resource')
+            resource.setAttribute('identifier', `${section.link}-resource`)
+            resource.setAttribute('type', 'webcontent')
+            resource.setAttribute('adlcp:scormtype', 'sco')
+            resource.setAttribute('href', filePath)
+            resource.appendChild(file)
+
+        let resources = manifest.getElementsByTagName('resources')[0]
+            resources.appendChild(resource)
+
+        let itemTitle = manifest.createElement('title')
+            itemTitle.innerHTML = section.title
+
+        let item = manifest.createElement('item')
+            item.setAttribute('identifier', `${section.link}-item`)
+            item.setAttribute('identifierref', `${section.link}-resource`)
+            item.appendChild(itemTitle)
+        
+
+        let resourceFile = html.cloneNode(true)
+
+            resourceFile.title = section.title
+
+            resourceFile.querySelector('#cover h2').innerHTML = section.title
+            resourceFile.querySelector('#cover p').innerHTML = await fetch(url + 'sub-title.md')
+                .then(response => response.text())
+                .catch( err => {throw err})
+            
+            resourceFile.querySelector('#content').setAttribute('data-markdown', url + '01.md')
+
+        const htmlContent = resourceFile.documentElement.outerHTML
+
+        zip.file(filePath, htmlContent)
+
+        return item
+
+    }else{
+
+        let title = manifest.createElement('title')
+            title.innerHTML = section.title
+
+        let item = manifest.createElement('item')
+            item.setAttribute('identifier', fileNameEncode(section.title))
+            item.appendChild(title)
+        
+            const appendedItems = section.sections.map( section => addSCOs(section,topic,manifest,html,zip,org,lang))
+            
+        
+        return await Promise.all(appendedItems).then( appendedItems => { 
+            appendedItems.forEach( items => item.appendChild(items))
+            return item
+        })
+    }
+}
+
+
+export function createMultiScorm(section, topic, org = "unfoldingWord", lang = "en", version = "1.0",){
+
+    const courseName = fileNameEncode(section.title)
+    console.log(section, topic, org, lang, courseName)
+    fetch('./src/multi-sco.zip')       // 1) fetch the url
+    .then(function (response) {                       // 2) filter on 200 OK
+        if (response.status === 200 || response.status === 0) {
+            return Promise.resolve(response.blob());
+        } else {
+            return Promise.reject(new Error(response.statusText));
+        }
+    })
+    .then(jszip.loadAsync)                            // 3) chain with the zip promise
+    .then( async (zip) => {
+        
+        var parser = new DOMParser();
+
+        let index = await zip.file("shared/template.html").async("string"); // 4) chain with the text content promise   
+        let parsedIndex = parser.parseFromString(index, "text/html")
+        let manifest = await zip.file("imsmanifest.xml").async("string"); // 4) chain with the text content promise
+        let parsedManifest = parser.parseFromString(manifest, "application/xml")
+
+        let manifestTag = parsedManifest.getElementsByTagName('manifest')[0]
+            manifestTag.setAttribute('identifier', courseName)
+            manifestTag.setAttribute('version', version)
+        
+        let organizations = parsedManifest.getElementsByTagName('organizations')[0]
+            organizations.setAttribute('default', courseName)
+
+        let title = parsedManifest.createElement('title')
+            title.innerHTML = section.title
+
+        let organization = parsedManifest.getElementsByTagName('organization')[0]
+            organization.setAttribute('identifier', courseName)
+            organization.appendChild(title)
+
+
+        const items =  section.sections.map( section  => addSCOs(section, topic, parsedManifest, parsedIndex, zip, org, lang) )
+        Promise.all(items).then( item => {
+            
+            const finished = item.map( item =>{console.log(item); organization.appendChild(item)})
+            if(finished){
+
+                let finalManifest = parsedManifest.documentElement.outerHTML
+                zip.file('imsmanifest.xml', finalManifest)
+                zip.generateAsync({type:"blob", compression: "DEFLATE", compressionOptions: { level: 9 } })
+                   .then(function (blob) {
+                        const zipName = `${courseName}.pif.zip`
+                        saveAs(blob, zipName)
+                    }) 
+
+            }
+        })
+        
+    })
+}
